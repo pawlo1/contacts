015f1bfa0c282b86e35f03a9b9f89e53d643cf4a
pawlo1 <pawlo1337@gmail.com>
2017-03-27 13:42:10 +0200

    1

---
+++ www/js/pgb.js
@@ -0,0 +1,24 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+console.log(navigator.contacts);
+}
+
+function onSuccess(contacts) {
+    alert('Found ' + contacts.length + ' contacts.');
+};
+ 
+function onError(contactError) {
+    alert('onError!');
+};
+ 
+
+var options      = new ContactFindOptions();
+options.filter   = "*";
+options.multiple = true;
+options.desiredFields = [navigator.contacts.fieldType.id];
+options.hasPhoneNumber = true;
+var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+navigator.contacts.find(fields, onSuccess, onError, options);
